eabf5ddc5bf173747d101345db1815b3fb9620ac
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-03 18:14:46 +0100

    Test commit #2

--- mongo_connect_testing.py
+++ mongo_connect_testing.py
@@ -3,7 +3,7 @@ import psycopg2
 
 #groet Ayoub
 #connect to the db
-con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123')
+con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=test123')
 
 # informatie tonen over wat data
 db = MongodbDAO.getMongoDB()
@@ -20,41 +20,39 @@ products = MongodbDAO.getDocuments("products")
 profiles = MongodbDAO.getDocuments("profiles")
 #products is een Cursor
 
-counter = 0
-for profile in profiles:
-	counter+=1
-	if counter < 100:
-		id = str(profile["_id"])
-		# id = str(profileID)
-		if "order" in profile.keys():
-			if "recommendations" in profile.keys():
-				try:
-					cur.execute("INSERT INTO profile (_id, ordercount, segment) VALUES (%s, %s, %s)", (id, profile["order"]["count"],profile["recommendations"]["segment"]))
-				except Exception as e:
-					# print(counter, id)
-					print(counter, e)
-			else:
-				try:
-					cur.execute("INSERT INTO profile (_id, ordercount) VALUES (%s, %s)", (id, profile["order"]["count"]))
-				except Exception as e:
-					print(counter, e)	
 
+all_counter = 0
+order_counter = 0
+seg_counter = 0
+for profile in profiles:
+	id = str(profile["_id"])
+	# id = str(profileID)
+	if "order" in profile.keys():
+		order_counter += 1
+		if "recommendations" in profile.keys():
+			seg_counter += 1
+			try:
+				cur.execute("INSERT INTO profile (_id, ordercount, segment) VALUES (%s, %s, %s)", (id, profile["order"]["count"],profile["recommendations"]["segment"]))
+			except Exception as e:
+				# print(counter, id)
+				print(e)
 		else:
-			if "recommendations" in profile.keys():
-				try:
-					cur.execute("INSERT INTO profile (_id, segment) VALUES (%s, %s)", (id,profile["recommendations"]["segment"]))
-				except Exception as e:
-					print(counter, e)
-			else:
-				try:
-					cur.execute("INSERT INTO profile (_id) VALUES (%s)", (id))
-				except Exception as e:
-					print(counter, e, id)
+			try:
+				cur.execute("INSERT INTO profile (_id, ordercount) VALUES (%s, %s)", (id, profile["order"]["count"]))
+			except Exception as e:
+				print(e)	
 	else:
-		print("Done!")
-		cur.execute("SELECT * FROM profile")
-		cur.fetchall()
-		break
+		if "recommendations" in profile.keys():
+			seg_counter += 1
+			try:
+				cur.execute("INSERT INTO profile (_id, segment) VALUES (%s, %s)", (id,profile["recommendations"]["segment"]))
+			except Exception as e:
+				print(e)
+		else:
+			try:
+				cur.execute("INSERT INTO profile (_id) VALUES (%s)", (id))
+			except Exception as e:
+				print(e)
 cur.close()
 
 
