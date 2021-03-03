c80f76a3259d3143d9944ab3d8bd8cc5d6f40ed0
MriLevi <74299880+MriLevi@users.noreply.github.com>
2021-03-03 16:46:26 +0100

    dit werkt nu wel

--- mongo_connect_testing.py
+++ mongo_connect_testing.py
@@ -18,21 +18,34 @@ cur = con.cursor()
 products = MongodbDAO.getDocuments("products")
 profiles = MongodbDAO.getDocuments("profiles")
 #products is een Cursor
-
-for profile in profiles:
-	id = str(profile["_id"])
-	if "order" in profile.keys():
-		if "count" in profile["order"].keys():
-			if "recommendations" in profile.keys():
-				try:
-					cur.execute("INSERT INTO profile (_id, ordercount, segment) VALUES (%s, %s, %s)", (id, profile["order"]["count"], profile["recommendations"]["segment"]))
-				except Exception as e:
-					print(id, e)
+def profile_converter():
+	'''This function converts a mongoDB profile entry into an SQL Profile table entry
+		it checks which information is available and inserts it correspondingly
+		it also prints a teringbende'''
+	for profile in profiles:
+		print(profile)
+		id = str(profile["_id"])
+		if "order" in profile.keys():
+			if "count" in profile["order"].keys():
+				if "recommendations" in profile.keys():
+					try:
+						cur.execute("INSERT INTO profile (_id, ordercount, segment) VALUES (%s, %s, %s)", (id, profile["order"]["count"], profile["recommendations"]["segment"]))
+					except Exception as e:
+						print(id, e)
+				else:
+					try:
+						cur.execute("INSERT INTO profile (_id, ordercount) VALUES (%s, %s)", (id, profile["order"]["count"]))
+					except Exception as e:
+						print(id, e)
 			else:
-				try:
-					cur.execute("INSERT INTO profile (_id, ordercount) VALUES (%s, %s)", (id, profile["order"]["count"]))
-				except Exception as e:
-					print(id, e)
+				if "recommendations" in profile.keys():
+					try:
+						cur.execute("INSERT INTO profile (_id, segment) VALUES (%s, %s)",
+									(id, profile["recommendations"]["segment"]))
+					except Exception as e:
+						print(id, e)
+				else:
+					continue
 		else:
 			if "recommendations" in profile.keys():
 				try:
