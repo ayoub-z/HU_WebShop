c5c31677247fc4095cc12c24f9dde77b950fb1eb
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-03-04 16:58:48 +0100

    poging om Buid tabel te vullen

--- mongo_connect_testing.py
+++ mongo_connect_testing.py
@@ -2,13 +2,13 @@ import MongodbDAO
 import psycopg2
 
 #connect to the db
-con = psycopg2.connect('host=localhost dbname=huwebshop user=postgres password=Levidov123')
+con = psycopg2.connect('host=localhost dbname=hu_webshop user=postgres password=123')
 
 # informatie tonen over wat data
 db = MongodbDAO.getMongoDB()
-collectionsNames = db.list_collection_names()
+collectionsNames = db.list_collection_names(no_cursor_timeout=True)
 for collectionName in collectionsNames:
-	collection = db.get_collection(collectionName)
+	collection = db.get_collection(collectionName,)
 
 
 #cursor
@@ -17,22 +17,38 @@ cur = con.cursor()
 #zoeken
 products = MongodbDAO.getDocuments("products")
 profiles = MongodbDAO.getDocuments("profiles")
-#products is een Cursor
+sessions = MongodbDAO.getDocuments("sessions")
 
-for profile in profiles:
-	id = str(profile["_id"])
-	if "order" in profile.keys():
-		if "count" in profile["order"].keys():
-			if "recommendations" in profile.keys():
-				try:
-					cur.execute("INSERT INTO profile (_id, ordercount, segment) VALUES (%s, %s, %s)", (id, profile["order"]["count"], profile["recommendations"]["segment"]))
-				except Exception as e:
-					print(id, e)
+
+#products is een Cursor
+def profile_converter():
+	'''This function converts a mongoDB profile entry into an SQL Profile table entry
+		it checks which information is available and inserts it correspondingly
+		it also prints a teringbende'''
+	for profile in profiles:
+		print(profile)
+		id = str(profile["_id"])
+		if "order" in profile.keys():
+			if "count" in profile["order"].keys():
+				if "recommendations" in profile.keys():
+					try:
+						cur.execute("INSERT INTO profile (_id, ordercount, segment) VALUES (%s, %s, %s)", (id, profile["order"]["count"], profile["recommendations"]["segment"]))
+					except Exception as e:
+						print(id, e)
+				else:
+					try:
+						cur.execute("INSERT INTO profile (_id, ordercount) VALUES (%s, %s)", (id, profile["order"]["count"]))
+					except Exception as e:
+						print(id, e)
 			else:
-				try:
-					cur.execute("INSERT INTO profile (_id, ordercount) VALUES (%s, %s)", (id, profile["order"]["count"]))
-				except Exception as e:
-					print(id, e)
+				if "recommendations" in profile.keys():
+					try:
+						cur.execute("INSERT INTO profile (_id, segment) VALUES (%s, %s)",
+									(id, profile["recommendations"]["segment"]))
+					except Exception as e:
+						print(id, e)
+				else:
+					continue
 		else:
 			if "recommendations" in profile.keys():
 				try:
@@ -43,63 +59,27 @@ for profile in profiles:
 			else:
 				continue
 
+def buidtablebuilder():
+	'''This function converts a mongoDB profile entry into an SQL Profile table entry
+		it checks which information is available and inserts it correspondingly'''
+	cur.execute("select _id from profile")
+	data = cur.fetchall()
+	usable__profile_id_list = []
 
+	for entry in data:
+		usable__profile_id_list.append(entry[0])
+	for profile in profiles:
+		id = str(profile["_id"])
+		if id in usable__profile_id_list:
+			if "buids" in profile.keys():
+				for buids in profile["buids"]:
+					print(f'id: {id}  buid: {buids}')
+					try:
+						cur.execute("INSERT INTO buid (_buid, profile_id) VALUES (%s, %s)",(buids, id))
+					except:
+						print(f'dr is iets kapot {buids}, {id}')
+
+buidtablebuilder()
 con.commit()
-
 cur.close()
 con.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for product in products:
-# 	if counter < 3:
-# 		print('Category:',product['category'])
-# 		print('Gender:',product['gender'])
-# 		print('Fast Mover:',product['fast_mover'])
-# 		print('Selling Price:',product['price']['selling_price'])
-# 		print('Doelgroep:',product['properties']['doelgroep'])
-# 		counter += 1
-# 		print('\n')
-# 	else:
-# 		print('done!')
-# 		break
-
-
-
-# prijzen = 0
-# counter = 0
-
-# for product in products:
-# 	try:
-# 		if type(product['price']['selling_price']) == int and product['price']['selling_price'] > 0:
-# 			prijzen += product['price']['selling_price']
-# 			counter += 1
-# 	except KeyError:
-# 		continue
-
