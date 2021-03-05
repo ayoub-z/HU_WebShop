2b2c47659b2c135e520b1e7647e3f585a84e8d32
MriLevi <74299880+MriLevi@users.noreply.github.com>
2021-03-05 12:11:34 +0100

    viewed_before_filler aangemaakt added commenting

--- mongo_connect_testing.py
+++ mongo_connect_testing.py
@@ -59,9 +59,12 @@ def profile_converter():
 
 def productconverter():
 	'''this converter converts the MongoDB Product document into a table
-	in postgreSQL, '''
+	in postgreSQL'''
+
 	skipcounter =0
+
 	for product in products:
+
 		### all the non-nullable variables: ###
 
 		#_id
@@ -111,6 +114,7 @@ def productconverter():
 			continue
 
 		###nullable variables###
+		#we try to set these and if they're not available in mongodb, we set them to None
 		try:
 			brand = product['brand']
 		except KeyError:
@@ -146,72 +150,85 @@ def productconverter():
 		 sub_sub_category, sub_sub_sub_category))
 
 def previously_recommended_filler():
+	'''This function fills the previous_recommended table in SQL with values from mongoDB '''
 
+	# get documents, filtered by id and previously_recommended
 	filterprev = {"_id":1, "previously_recommended":1}
 	profiles_with_prev = MongodbDAO.getCollection("profiles").find({}, filterprev, no_cursor_timeout=True)
+
+	#counters
 	skipcounter=0
 	insertcounter=0
 	private_key_counter=1
 	sqlerrorcounter = 0
+
+	#loop through all the profiles
 	for profile in profiles_with_prev:
-		id = str(profile["_id"])
-		if "previously_recommended" not in profile.keys():
+		id = str(profile["_id"]) #convert id to string
+		if "previously_recommended" not in profile.keys(): #if no prev, skip entry
 			continue
 		else:
-			for recommendation in profile["previously_recommended"]:
-				try:
+			for recommendation in profile["previously_recommended"]: #for every product id in prev
+				try:												 #try to insert it in SQL database
 					print(f'inserting profile_id: {profile["_id"]}, reccomendation: {recommendation}')
 					cur.execute("INSERT INTO previously_recommended (previously_recommended_id, profile_id, product_id) VALUES (%s, %s, %s)",
 							(private_key_counter, id, recommendation))
 					con.commit()
-					insertcounter += 1
-					private_key_counter += 1
+					insertcounter += 1 			#count amount of inserts
+					private_key_counter += 1	#simple private key counter
 				except psycopg2.errors.ForeignKeyViolation:
-					print(f'product id:{recommendation} niet nuttig, skip')
+					print(f'product id:{recommendation} niet nuttig, skip') #if we find a product thats not in SQL, skip
 					skipcounter+=1
 					continue
-				except psycopg2.errors.InFailedSqlTransaction:
+				except psycopg2.errors.InFailedSqlTransaction:				#sometimes SQL transaction fails, except it
 					print(f'hier komen we een sql transactie error tegen, skip')
-					sqlerrorcounter+=1
-					con.rollback()
-	con.commit()
+					sqlerrorcounter+=1										#keep count of how often this happens
+					con.rollback()										    #look at this if it's significant amount
+	con.commit()															#(right now its only ~500 out of 1.4m inserts)
 	cur.close()
 	con.close()
 	print(f'done! skipped:{skipcounter}, inserted: {insertcounter}, sql errors: {sqlerrorcounter}')
 
 
 def viewed_before_filler():
+	'''This function fills the viewed_before table in SQL with values from mongoDB'''
+
+	#get documents, filtered by id and recommendations
 	filterprev = {"_id": 1, "recommendations":1}
 	profiles_with_recommendations = MongodbDAO.getCollection("profiles").find({}, filterprev, no_cursor_timeout=True)
+
+	#a few counters
 	skipcounter = 0
 	insertcounter = 0
 	private_key_counter = 1
 	sqlerrorcounter = 0
-	
+
+	#loop through all the profiles
 	for profile in profiles_with_recommendations:
-		id = str(profile["_id"])
-		if "recommendations" not in profile.keys():
+		id = str(profile["_id"]) 					#convert profile to string instead of objectid
+		if "recommendations" not in profile.keys(): #if theres no recommendations, skip
 			continue
-		if "viewed_before" not in profile["recommendations"]:
+		if "viewed_before" not in profile["recommendations"]: #if we do have recommendations but no viewed before, skip
 			continue
 		else:
-			for viewedproduct in profile["recommendations"]["viewed_before"]:
-				try:
+			for viewedproduct in profile["recommendations"]["viewed_before"]:	#for every product thats in viewed before
+				try:														    #try to insert it
 					print(f'inserting profile_id: {profile["_id"]}, reccomendation: {viewedproduct}')
 					cur.execute(
 						"INSERT INTO viewed_before (viewed_before_id, profile_id, product_id) VALUES (%s, %s, %s)",
 						(private_key_counter, id, viewedproduct))
 					con.commit()
-					insertcounter += 1
-					private_key_counter += 1
-				except psycopg2.errors.ForeignKeyViolation:
+					insertcounter += 1		#keep track of succesful inserts
+					private_key_counter += 1#we use this to generate a simple private key
+				except psycopg2.errors.ForeignKeyViolation: #if we find a product id thats not in our product table, skip
 					print(f'product id:{viewedproduct} niet nuttig, skip')
 					skipcounter += 1
 					continue
-				except psycopg2.errors.InFailedSqlTransaction:
+				except psycopg2.errors.InFailedSqlTransaction: #sometimes SQL transactions fail, count them
 					print(f'hier komen we een sql transactie error tegen, skip')
 					sqlerrorcounter += 1
-					con.rollback()
+					con.rollback() #this rolls back the transaction and makes sure on the next commit we
+								   #wont try to commit the faulty transaction again.
 	con.commit()
 	cur.close()
 	con.close()
