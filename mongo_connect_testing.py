ae931dfb52a6dfa4e512a3e84f09e6a6fe6390ca
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-05 18:58:09 +0100

    Reomved unnecessary code

--- mongo_connect_testing.py
+++ mongo_connect_testing.py
@@ -60,67 +60,6 @@ def profile_converter():
 			else:
 				continue
 
-
-
-# def session_filler():
-
-# 	counter = 0
-# 	unique_exceptionError = 0
-# 	ForeignKey_exceptionError = 0
-# 	buid_anomalie = 0
-# 	overige_error = 0
-# 	no_order = 0
-
-
-# 	for session in sessions:
-
-# 		try:
-# 			session_id = str(session["_id"])
-# 		except:
-# 			print('geen id')
-
-# 		try:
-# 			session_buid = str(session["buid"])
-# 		except:
-# 			print("Session buid invalid")
-
-# 		try:
-# 			sale = session["has_sale"]
-# 		except:
-# 			print("Geen sale")
-
-# 		try:
-# 			order = session["order"]
-# 			if order == None:
-# 				continue
-# 		except:
-# 			no_order += 1
-# 			print(f'Geen order')
-
-# 		try:
-# 			cur.execute('INSERT INTO "session" (_id, buid_buid, has_sale) VALUES (%s, %s, %s)', 
-# 															(session_id, session_buid, sale))
-# 			counter += 1
-# 			print(counter)
-# 			con.commit()
-# 		except psycopg2.errors.UniqueViolation:	
-# 			unique_exceptionError += 1
-# 			print(f'{unique_exceptionError} Exception used on {session_id}, {session_buid}')
-# 			con.rollback()
-# 		except psycopg2.errors.ForeignKeyViolation:
-# 			ForeignKey_exceptionError += 1
-# 			print('Buid does not exist. skipping', ForeignKey_exceptionError)
-# 			con.rollback()
-# 		except psycopg2.errors.StringDataRightTruncation:
-# 			buid_anomalie += 1
-# 			con.rollback()
-# 			print(f'Kapot')
-# 		except psycopg2.errors.InFailedSqlTransaction:
-# 			overige_error += 1
-# 			con.rollback()
-# 			print('kapot: overig')
-# 	print(f"Foreignkey Violations: {ForeignKey_exceptionError} \nUnique Violations {unique_exceptionError} \nTe veel buids: {buid_anomalie} \nOverige Errors: {overige_error} \nGeen Orders: {no_order}")
-
 def session_filler():
 
     insert_counter = 0
@@ -178,123 +117,6 @@ def session_filler():
     print(f'Done! We inserted {insert_counter} documents, got unique exception: {unique_exceptionError_counter} times')
     print(f'Got foreign key error {ForeignKey_exceptionError_counter} times, Too long buid error: {buid_Exception_counter} times, and failed {failed_sql_counter} sql queries.')
 
-
-# def order_filler():
-
-#     insert_counter = 0
-#     unique_exceptionError_counter = 0
-#     ForeignKey_exceptionError_counter = 0
-#     buid_Exception_counter = 0
-#     failed_sql_counter= 0
-#     count = 0
-
-
-#     for session in sessions:
-
-#         try:
-#             session_id = str(session["_id"])
-#         except:
-#             print('geen id')
-#         try:
-#             order = session["order"]
-#             if order == None:
-#                 continue
-#         except:
-#             print(f'Geen order')
-
-#         try:
-#             count += 1
-#             cur.execute('INSERT INTO "order" (orderid, session_id) VALUES (%s, %s)',
-#                         (count, session_id))
-#             insert_counter += 1
-#             print(insert_counter)
-#             con.commit()
-#         except psycopg2.errors.UniqueViolation:
-#             unique_exceptionError_counter += 1
-#             print(f'{unique_exceptionError_counter} Exception used on {count}, {session_id}')
-#             con.rollback()
-#         except psycopg2.errors.ForeignKeyViolation:
-#             ForeignKey_exceptionError_counter += 1
-#             print('Buid does not exist. skipping', ForeignKey_exceptionError_counter)
-#             con.rollback()
-#         except psycopg2.errors.StringDataRightTruncation:
-#             buid_Exception_counter += 1
-#             con.rollback()
-#         except psycopg2.errors.InFailedSqlTransaction:
-#             failed_sql_counter += 1
-#             con.rollback()
-#     print(f'Done! We inserted {insert_counter} documents, got unique exception: {unique_exceptionError_counter} times')
-#     print(f'Got foreign key error {ForeignKey_exceptionError_counter} times, Too long buid error: {buid_Exception_counter} times, and failed {failed_sql_counter} sql queries.')
-
-def order_filler_failed():
-	'''This function converts the mongoDB session entry 'order' into the SQL 'order' table 
-		it checks which information is available and inserts it correspondingly'''
-	cur.execute('SELECT _id FROM "session"') 
-	session_ids = cur.fetchall() #alle session_ids in de database
-	session_ids_list = [] #list waarin alle session_ids komen die in de database zijn 
-	
-	count = 0 #houdt bij het aantal orders dat niet leeg is
-			  #wordt ook meegegeven als orderid
-	insert_counter = 0
-	
-	for entry in session_ids:
-		session_ids_list.append(entry[0]) #session_ids uit database op een bruikbare manier in list brengen.
-
-	for session in sessions:
-		session_id = session["_id"]
-		if session_id in session_ids_list: #checkt of session_id al in de database zit
-			if session["has_sale"] == True:
-				if "order" in session.keys() and session["order"] != None:
-					count+=1					
-					try: #voegt in de database de orderid en session_id
-						cur.execute('INSERT INTO "order"(orderid, session_id) VALUES (%s, %s)', 
-																			(count, session_id))
-						insert_counter += 1
-						print('inserts:',insert_counter)
-						con.commit()
-					except Exception as e:
-						print(e)
-				else:
-					continue
-			else:
-				continue
-		else:
-			continue
-
-
-# def product_order_filler1():
-# 	'''This function converts the mongoDB session entry 'products' into the SQL 'product_order' table
-# 		it checks which information is available and inserts it correspondingly'''
-# 	cur.execute('SELECT session_id FROM "order"')
-# 	session_ids = cur.fetchall() #alle session_ids in de database
-# 	session_ids_list = [] #list waarin alle session_ids komen die in de database zijn 
-	
-# 	count = 0 #houdt bij het aantal orders dat gekochten producten bevat
-# 			#wordt ook meegegeven als product_ordeer_id en orderoerderid
-# 	# for entry in session_ids:
-# 	#     session_ids_list.append(entry[0]) #session_ids uit database op een bruikbare manier in list brengen.
-
-# 	for session in sessions:
-# 		if session in session_ids_list: #checkt of session_id al in de database zit
-# 			if "order" in session.keys():
-# 				if session["order"] !=None and "products" in session["order"].keys(): #checkt of er ook werkelijk producten zitten in order
-# 					product_orders = session["order"]["products"] #alle producten in een order
-# 					count += 1
-# 					for product in product_orders:					
-# 						try: #voegt in de database de product_order_id, product_id en orderid
-# 							cur.execute("INSERT INTO product_order(product_order_id, product_id, orderorderid) \
-# 																VALUES (%s, %s, %s,)", (count, product, count)) 
-# 							print(count)
-# 							con.commit()
-# 																# note sure if it will be linked to exact same order
-# 						except Exception as e:
-# 							print(e)
-# 				else:
-# 					continue
-# 			else:
-# 				continue
-# 		else:
-# 			continue
 
 
 def order_filler():
@@ -385,43 +207,9 @@ def buidtablebuilder():
 
 
 
-def product_order_filler2():
-	'''This function converts the mongoDB session entry 'products' into the SQL 'product_order' table
-		it checks which information is available and inserts it correspondingly'''
-	# cur.execute('SELECT session_id FROM "order"')
-	# session_ids = cur.fetchall() #alle session_ids in de database
-	# session_ids_list = [] #list waarin alle session_ids komen die in de database zijn 
-	
-	count = 0 #houdt bij het aantal orders dat gekochten producten bevat
-			#wordt ook meegegeven als product_ordeer_id en orderoerderid
-	# for entry in session_ids:
-	#     session_ids_list.append(entry[0]) #session_ids uit database op een bruikbare manier in list brengen.
-
-	for session in sessions:
-		if "order" in session.keys():
-			if session["order"] !=None and "products" in session["order"].keys(): #checkt of er ook werkelijk producten zitten in order
-				product_orders = session["order"]["products"] #alle producten in een order
-				count += 1
-				for product in product_orders:					
-					try: #voegt in de database de product_order_id, product_id en orderid
-						cur.execute("INSERT INTO product_order(product_order_id, product_id, orderorderid) \
-															VALUES (%s, %s, %s,)", (count, product, count)) 
-						print(count)												# not sure if it will be linked to exact same order
-						con.commit()
-															
-					except Exception as e:
-						print(e)
-			else:
-				continue
-		else:
-			continue
 
 
-
-
-
-
-def product_order_filler3():
+def product_order_filler():
 	for session in sessions:
 
 		try:
