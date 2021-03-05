1ffccfbf3d55afbef2553408641cd64ba19edc00
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-03-05 10:46:40 +0100

    Session+order func done. Added product_order func

--- mongo_connect_testing.py
+++ mongo_connect_testing.py
@@ -18,10 +18,9 @@ cur = con.cursor()
 products = MongodbDAO.getDocuments("products")
 profiles = MongodbDAO.getDocuments("profiles")
 sessions = MongodbDAO.getDocuments("sessions")
-product_orders = MongodbDAO.getDocuments("sessions",{'orders': 'products'})
 
 
-#products is een Cursor
+
 def profile_converter():
 	'''This function converts a mongoDB profile entry into an SQL Profile table entry
 		it checks which information is available and inserts it correspondingly
@@ -32,6 +31,7 @@ def profile_converter():
 			if "count" in profile["order"].keys():
 				if "recommendations" in profile.keys():
 					try:
+						print("1")
 						cur.execute("INSERT INTO profile (_id, ordercount, segment) VALUES (%s, %s, %s)", (profile_id, profile["order"]["count"], profile["recommendations"]["segment"]))
 					except Exception as e:
 						print('1 ', e)
@@ -61,74 +61,75 @@ def profile_converter():
 
 
 
-
 def session_insert():
-	count = 0
 	cur.execute("SELECT buid FROM buid")
 	buid_buid = cur.fetchall()
 	buid_list = []
 	for session in sessions:
-		if count < 100:
-			count += 1
-			session_id = str(session["_id"])
-			session_buid = session["buid"]
-			sale = session["has_sale"]
-			for entry in buid_buid:
-				buid_list.append(entry[0])
+		count += 1
+		session_id = str(session["_id"])
+		session_buid = session["buid"]
+		sale = session["has_sale"]
+		for entry in buid_buid:
+			buid_list.append(entry[0])
 			if session["buid"] in buid_list:
-				if "has_sale" in session.keys():
-					if sale == True:
+				if sale == True:
+					try:
+						cur.execute("INSERT INTO session (_id, buid_buid, has_sale) VALUES (%s, %s, %s)", (session_id, session_buid, sale))
+					except Exception as e:
+						print(e)
+				else:
+					continue
+			else:
+				continue
+
+
+
+def order_insert():
+	'''This function converts the mongoDB session entry 'order' into the SQL 'order' table 
+		it checks which information is available and inserts it correspondingly'''
+	count = 0
+	for session in sessions:
+		session_id = str(session["_id"])
+		if session["has_sale"] == True:
+			if "order" in session.keys() and session["order"] != None:
+				count+=1					
+				try:
+					cur.execute('INSERT INTO "order"(orderid, session_id) VALUES \
+												(%s, %s, %s)', (count, session_id))
+				except Exception as e:
+					print(e)
+			else:
+				continue
+		else:
+			continue
+
+
+
+def product_order_insert():
+	'''This function converts the mongoDB session entry 'products' into the SQL 'product_order' table
+		it checks which information is available and inserts it correspondingly'''
+	count = 0
+	for session in sessions:
+		if session["has_sale"] == True:
+			if "order" in session.keys():
+				if session["order"] !=None and "products" in session["order"].keys():
+					product_orders = session["order"]["products"]
+					count += 1
+					for product in product_orders:					
 						try:
-							cur.execute("INSERT INTO session(_id, buid_buid, has_sale) VALUES (%s, %s, %s)", (session_id, session_buid, sale))
+							cur.execute("INSERT INTO product_order(product_order_id, product_id, orderorderid) \
+																VALUES (%s, %s, %s,)", (count, product, count))
 						except Exception as e:
 							print(e)
-					else:
-						continue
 				else:
 					continue
 			else:
 				continue
 		else:
-			break
+			continue
 
+# con.commit()
+# cur.close()
+# con.close()
 
-
-# #Similar code, inteded for testing until buid table is setup
-
-# count = 0
-# for session in sessions:
-# 	if count < 100:
-# 		count += 1
-# 		session_id = str(session["_id"])
-# 		session_buid = str(session["buid"])
-# 		sale = session["has_sale"]
-# 		if "has_sale" in session.keys():
-# 			if sale == True:
-# 				try:
-# 					print('yes!')
-# 					cur.execute("INSERT INTO session(_id, buid_buid, has_sale) VALUES (%s, %s, %s)", (session_id, session_buid, sale))
-# 				except Exception as e:
-# 					print('no', e)
-# 			else:
-# 				continue
-# 		else:
-# 			continue
-# 	else:
-# 		break
-
-
-def order_insert():
-	count = 0
-	for session in sessions:
-		session_id = str(session["_id"])
-		if "has_sale" in session.keys():
-			if session["has_sale"] == True:
-				for product in product_orders:
-					if "Products" in product.keys():
-						count+=1
-						for product in product_orders:
-							try:
-								print(count, product)
-								cur.execute("INSERT INTO order(orderid, Product_id, session_id) VALUES (%s, %s, %s)", (count, product, session_id))
-							except Exception as e:
-								print(e)
