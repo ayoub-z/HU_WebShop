56b36f94fcf2ee6459fb08aaac820fff9517d20b
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-03-15 15:51:17 +0100

    Seems to be working. Not fully tested

--- Reccommendation.py
+++ Reccommendation.py
@@ -57,6 +57,11 @@ def prod_tablemaker():
         print(SQL_skeerie)
     con.commit()
 
+    # Also makes another table I couldn't be bothered to make anywhere else
+    # Create table for profile based reco's
+    cur.execute("Create Table VB_reco (profile varchar(255) NOT NULL PRIMARY KEY, reco1 varchar(255), reco2 varchar(255), reco3 varchar(255), reco4 varchar(255) );")
+    con.commit()
+
 def table_elimination():
     '''function to cascade drop delete eliminate destroy tables made in function tablemaker'''
     cur.execute("DROP TABLE IF EXISTS brandsub_category ; DROP TABLE IF EXISTS brandsub_categorysub_sub_category CASCADE;  DROP TABLE IF EXISTS categorybrand CASCADE;  DROP TABLE IF EXISTS categorybrandsub_category CASCADE; DROP TABLE IF EXISTS categorybrandsub_categorysub_sub_category CASCADE; DROP TABLE IF EXISTS categorybrandsub_sub_category CASCADE;DROP TABLE IF EXISTS categorysub_category CASCADE; DROP TABLE IF EXISTS categorysub_categorysub_sub_category CASCADE; DROP TABLE IF EXISTS categorysub_sub_category CASCADE; DROP TABLE IF EXISTS sub_categorysub_sub_category CASCADE;  DROP TABLE IF EXISTS brandsub_sub_category CASCADE;")
@@ -120,7 +125,8 @@ def categorybrandsub_categorysub_sub_category_filler():
             # This try & except picks 4 random entries from the similiar_prod_list, and inserts them into the corresponding table. If there's an error, continue to the next product
             try:
                 four_prod = random.sample(similiar_prod_list, 4)
-            except:
+            except Exception as e:
+                print(f'Error {e} , when trying to select 4 random products from similiar_prod_list.')
                 continue
             #The remainder of this function is made to insert the propper data into postgreSQL
             inserttuple=[product[id],]
@@ -132,8 +138,87 @@ def categorybrandsub_categorysub_sub_category_filler():
                 con.commit()
                 print(f' succes met {product[id]}')
             except Exception as e:
+                con.rollback()
                 print(e)
 
-def
+def VB_reco_filler():
+    '''Function that finds all profiles that have viewed the same product as the given profile. ( limited to a single product from the original profile )
+    From that list of profiles, all products that those profiles have also viewed before will be made, with a counter for each product.
+    Function returns the 4 most viewed products found, excluding the original product.
+    VB is an abreviation I used for  "Viewed Before "         '''
+
+    profileid=('5a393eceed295900010386a8',)
+    #Try & except to try and retrieve a singular product from the given profile.
+    try:
+        cur.execute("SELECT product_id FROM viewed_before WHERE profile_id = %s",profileid)
+        VBproduct = cur.fetchone()
+    except:
+        print("Error in het ophalen van product_id uit het originele profile.")
+        con.rollback()
+
+    #Try & except to try and retrieve all profile ID's that have viewed VBproduct before.
+    try:
+        cur.execute("SELECT profile_id FROM viewed_before where product_id = %s", VBproduct)
+        VB_prof_list = cur.fetchall()
+    except exception as e:
+        print(f'Error {e} , when trying to select profile IDs.')
+
+    #dictionairy product counter
+    also_VB_prod_count = {}
+
+    for profile in VB_prof_list:
+
+        #Try & except to try and retrieve all product ID's in profile.
+        try:
+            cur.execute("SELECT product_id FROM viewed_before where profile_id = %s", profile)
+            VB = cur.fetchall()
+        except Exception as e:
+            print(f'Error {e} , when trying to select product IDs contained in profile.')
+
+        #for-loop, adding all products in profile to also_VB_prod_count
+        for prod in VB:
+
+            #if product is original product, skip
+            if prod == VBproduct:
+                continue
+
+            #if product already in dict, add 1 to its value
+            if prod[0] in also_VB_prod_count:
+                also_VB_prod_count[prod[0]] += 1
+                print(" +1 life")
+
+            #if product not yet in dict, add key to dict and 1 to value
+            elif prod[0] not in also_VB_prod_count:
+                also_VB_prod_count[prod[0]] = 1
+                print(" new life")
+    #Reverse sort dict to be able to take the top 4 values.
+    sorted(also_VB_prod_count.items(), key=lambda x: x[1], reverse=True)
+
+    #create tuple that's
+    inserttuple=[profile[0]]
+    for prod in also_VB_prod_count:
+        if len(inserttuple)<5:
+            inserttuple.append(prod)
+        else:
+            break
+    inserttuple = tuple(inserttuple)
+    print(inserttuple)
+
+    #insert data into VB_RECO
+    try:
+        cur.execute('INSERT INTO "vb_reco" (profile, reco1, reco2, reco3, reco4) VALUES (%s, %s, %s, %s, %s)', inserttuple)
+        con.commit()
+    except Exception as e:
+        print(f'Error {e} , when trying to insert data into VB_reco.')
 
 
+
+
+
+
+
+
+
+
+
+VB_reco_filler()
